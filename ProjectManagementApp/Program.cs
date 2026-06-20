f77b1fce414b8082ceaf92b24329224a4abe223c
Azhar786786786 <talkazhar786@hotmail.com>
2026-06-20 10:41:39 +0530

    Added Packages and their configuration

--- ProjectManagementApp/Program.cs
+++ ProjectManagementApp/Program.cs
@@ -1,3 +1,6 @@
+using Scalar.AspNetCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +9,16 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+//db registering
+//builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference("/scalar");
 }
 
 app.UseHttpsRedirection();
